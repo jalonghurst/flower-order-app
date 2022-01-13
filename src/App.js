9f1d5fd8bcd43ba6f566e7affb2512beae26310d
jalonghurst <jacqui.longhurst@btinternet.com>
2022-01-13 20:35:12 +0100

    final additions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import {useState} from 'react';
-import Header from './components/Layout/Header';
-import Flowers from './components/Flowers/Flowers';
-import CartProvider from './store/CartProvider';
+import { useState } from "react";
+
+import Header from "./components/Layout/Header";
+import Flowers from "./components/Flowers/Flowers";
+import CartProvider from "./store/CartProvider";
+import Cart from "./components/Cart/Cart";
 
 function App() {
-  const [cartIsShown,setCartIsShown]= useState(false);
-  function showCartHandler () {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
     setCartIsShown(true);
-  }
-  function hideCartHandler() {
+  };
+
+  const hideCartHandler = () => {
     setCartIsShown(false);
-  }
+  };
   return (
     <CartProvider>
-      {cartIsShown && <div>Cart...</div>}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Cart />
       <Header onShowCart={showCartHandler} />
       <main>
         <Flowers />
